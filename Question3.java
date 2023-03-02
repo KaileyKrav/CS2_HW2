9c1f731b9b4c8cbbad9e4efd24b5d85aae20e1aa
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2023-03-02 18:20:04 -0500

    Create Question3.java

---
+++ Question3.java
@@ -0,0 +1,5 @@
+/*
+ * Take the airplane seating chart from the previous homework and recreate it using a multidimensional array.
+ * Initalize it with all seats as available and print it out.
+ * Now, mark the same seats as the previous hw as booked and reprint out the seating chart.
+ */
