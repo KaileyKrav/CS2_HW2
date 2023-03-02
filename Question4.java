9354217ee3ab95ce5d23135e2e2e02b579f508b2
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2023-03-02 18:22:37 -0500

    Create Question4.java

---
+++ Question4.java
@@ -0,0 +1,5 @@
+/*
+ * HARD JUST TRY YOUR BEST
+ * Do the same as the previous question, ask the user how many seats they need (no more than 4). Loop through the array and find the available seats they can book.
+ * If they are booking more than 1 seat, they want to sit in the same row, check the subsequent seats to see if they are booked and print out the available seats.
+ */
