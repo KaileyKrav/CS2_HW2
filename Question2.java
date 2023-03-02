dbcf8937bc57633eb0ea18f86dd87bdaa68bf104
Kailey <93210118+KaileyKrav@users.noreply.github.com>
2023-03-02 18:17:22 -0500

    Create Question2.java

---
+++ Question2.java
@@ -0,0 +1,6 @@
+/*
+ * Same as question 1, but also create another array of the students names. The scores in the first array correspond to the student name array.
+ * EX: Student[2] = Sarah and testScore[2] = 98.
+ * Print out the students' names and their scores.
+ * Ask the user to search a student and print out the corresponding score.
+ */
